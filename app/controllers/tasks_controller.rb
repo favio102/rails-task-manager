b99079e26d1caa3287925025be960f56a405c165
Favio Valero <favio102@protonmail.com>
2023-06-30 19:39:16 -0700

    refactor

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,10 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def new
@@ -12,23 +12,21 @@ class TasksController < ApplicationController
   end
 
   def create
-    task = Task.new(task_params)
+    @task = Task.find(params[:id])
     task.save
     redirect_to task_path(task)
   end
 
   def edit
-    @task = Task.find(params[:id])
   end
 
   def update
-    task = Task.find(params[:id])
     task.update(task_params)
     redirect_to task_path(task)
   end
 
   def destroy
-    @task = Task.find(params[:id])
+
     @task.destroy
     redirect_to tasks_path, status: :see_other
   end
@@ -37,5 +35,9 @@ class TasksController < ApplicationController
 
   def task_params
     params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
-  # Read all
-  get 'tasks', to: 'tasks#index'
-  # Create
-  get 'tasks/new', to: 'tasks#new'
-  post 'tasks', to: 'tasks#create'
-  # Read one
-  get 'tasks/:id', to: 'tasks#show', as: :task
-  # Update
-  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
-  patch 'tasks/:id', to: 'tasks#update'
-  # Delete
-  delete 'tasks/:id', to: 'tasks#destroy'
-  # resources :tasks, only: [:index, :show]
+  # # Read all
+  # get 'tasks', to: 'tasks#index'
+  # # Create
+  # get 'tasks/new', to: 'tasks#new'
+  # post 'tasks', to: 'tasks#create'
+  # # Read one
+  # get 'tasks/:id', to: 'tasks#show', as: :task
+  # # Update
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  # patch 'tasks/:id', to: 'tasks#update'
+  # # Delete
+  # delete 'tasks/:id', to: 'tasks#destroy'
+  resources :tasks
 end
